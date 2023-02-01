f3a6067266840ab35cf8835cced5f04b93fa5b29
Matt R <quiksillvr@yahoo.com>
2023-02-01 18:16:16 -0500

    Added robot protection

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-// import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
@@ -11,13 +10,17 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ContactComponent implements OnInit {
 
-  endpoint: string = "https://www.thurmontpaving.com/assets/other/sendEmail.php";
+  private endpoint: string = "https://www.thurmontpaving.com/assets/other/sendEmail.php";
+  private number_to_match = "8410";
+  private button_click_delay = 2000;
+  public is_disabled: boolean = false;
 
   public contactForm = this.formBuilder.group({
-    name: '',
-    email: '',
-    phone: '',
-    message: ''
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    message: ['', Validators.required],
+    random_numbers: ['']
   });
 
   constructor(
@@ -29,17 +32,30 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  public onSubmit(): void {
+  private enableButton(){
+    window.setTimeout(() => this.is_disabled = false, this.button_click_delay);
+  }
 
-    console.log(JSON.stringify(this.contactForm.value))
+  public onSubmit(): void {
+    // console.log(JSON.stringify(this.contactForm.value))
+    this.is_disabled = true;
+
+    if (this.contactForm.value.random_numbers !== this.number_to_match) {
+      this.toastr.error("Incorrect number entered, please try again");
+      this.enableButton();
+      return;
+    }
+
     this.httpClient.post(this.endpoint, this.contactForm.value).subscribe({
           next: __data => {
             this.toastr.success('Message sent, thank you.');
             this.contactForm.reset();
+            this.enableButton();
         },
         error: error => {
             this.toastr.error('Sorry we were unable to send your message at this time.');
             console.error('There was an error!', error);
+            this.enableButton();
         }
     })
 
